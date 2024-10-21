a0e33c7aadedfa9297ac2e34ea97ad83ee7e03ec
Anurag Singh Chauhan <dev.anuragchauhan@gmail.com>
2024-10-21 17:31:41 +0530

    updated ai-assistant package

--- debales-ai-assistant/debales-ai-assistant.php
+++ debales-ai-assistant/debales-ai-assistant.php
@@ -15,7 +15,7 @@
  * Plugin Name:       Debales AI Assistant
  * Plugin URI:        https://debales.ai/
  * Description:       This plugin will help you to integrate Debales AI Assistant Chatbot into your website.
- * Version:           1.10.0
+ * Version:           1.10.1
  * Requires at least: 5.2
  * Requires PHP:      7.2
  * Author:            Debales
